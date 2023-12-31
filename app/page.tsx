98bd072de7f63f036f2ea3c46b326ed432578fce
kislay-gupta <kislaygupta007@gmail.com>
2024-01-01 00:20:58 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">next 14 we&apos;re here </h1>
-      <h2 className="h2-bold">next 14 we&apos;re here </h2>
-      <h3 className="h3-bold">next 14 we&apos;re here </h3>
-    </div>
-  );
-}
